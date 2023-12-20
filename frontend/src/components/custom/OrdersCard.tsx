955ddaff878be57436203f1b99e05453ea6c1099
bhavin1324 <bjariwala26@gmail.com>
2023-12-20 14:50:45 +0530

    Order delivery flow completed

---
+++ frontend/src/components/custom/OrdersCard.tsx
@@ -0,0 +1,168 @@
+import { IOrder } from "../../types/interfaces";
+import { useEffect, useState } from "react";
+import { useFetch } from "../../hooks/useFetch";
+import { ApiEndpoints } from "../../types/enums";
+import Swal from "sweetalert2";
+import { useDispatch } from "react-redux";
+import { setProgress } from "../../features/slices/loadingSlice";
+function OrderCard({ order, loadData }: { order: any; loadData: () => void }) {
+  const dispatch = useDispatch();
+  const UpdateHook = useFetch(
+    import.meta.env.VITE_PREPARATION_SERVICE_URI + ApiEndpoints.DISPATCH_ORDER,
+    "POST"
+  );
+  const VerifyHook = useFetch(
+    import.meta.env.VITE_DELIVERY_SERVICE_URI + ApiEndpoints.SEND_OTP_TO_CUST,
+    "GET"
+  );
+  const DeliveryHook = useFetch(
+    import.meta.env.VITE_DELIVERY_SERVICE_URI +
+      ApiEndpoints.UPDATE_STATUS_TO_DELIVERED,
+    "GET"
+  );
+  const Toast = Swal.mixin({
+    toast: true,
+    position: "top-right",
+    iconColor: "white",
+    customClass: {
+      popup: "colored-toast",
+    },
+    showConfirmButton: false,
+    timer: 1500,
+    timerProgressBar: true,
+  });
+  const [OTP, setOTP] = useState(null);
+  useEffect(() => {}, []);
+
+  const formatPrice = (price: number) => `${price} Rs.`;
+  const verifyOrder = (id, orderid) => {
+    dispatch(setProgress(70));
+    document.getElementById("verifybtn").setAttribute("disabled", "disabled");
+    VerifyHook.MakeHttpRequest(id).then(async (result) => {
+      document.getElementById("verifybtn").removeAttribute("disabled");
+      if (result.result.status == 200) {
+        dispatch(setProgress(100));
+        Toast.fire({
+          title: "OTP Sent to Customer !",
+          icon: "success",
+        });
+
+        const tempotp = result.result.message;
+        console.log(tempotp);
+        const { value: OTPinput } = await Swal.fire({
+          title: "Confirm Customer OTP ! ",
+          input: "number",
+          inputLabel: "",
+          inputPlaceholder: "Enter 6 digit OTP !",
+        });
+        if (OTPinput.toString() == tempotp) {
+          DeliveryHook.MakeHttpRequest(orderid).then((result) => {
+            if (result.result.status == 200) {
+              Swal.fire("OTP Verified!", "Order is delivered !", "success");
+              loadData();
+            } else {
+              Toast.fire({
+                title: "Server Error, Try agin !",
+                icon: "error",
+              });
+            }
+          });
+        } else {
+          Swal.fire({
+            icon: "error",
+            title: "Verification Failed..",
+            text: "You have entered Wrong OTP !",
+          });
+        }
+      } else {
+        Toast.fire({
+          title: "Server Error !",
+          icon: "error",
+        });
+      }
+    });
+  };
+
+  const updateToCompleted = (id) => {
+    dispatch(setProgress(70));
+    const payload = {
+      outletid: localStorage.getItem("outlet"),
+      orderid: id,
+    };
+    UpdateHook.setPayload(payload);
+    UpdateHook.MakeHttpRequest().then((result) => {
+      if (result.result.status != 0) {
+        Toast.fire({
+          title: "Delivery Person Assigned !",
+          icon: "success",
+        });
+        loadData();
+        dispatch(setProgress(100));
+      } else {
+        Toast.fire({
+          title: "Error in Assigning Delivery Person !",
+          icon: "error",
+        });
+      }
+    });
+  };
+  return (
+    <div className="bg-white m-3 align-middle pt-3 px-3 flex-col max-w-fit  rounded shadow-md">
+      <h3 className="font-bold text-xl text-center text-ph-primary mb-2">
+        {order.name}'s Order
+      </h3>
+      <div className="h-fit">
+        {order.items.map((item) => (
+          <div key={item.name} className="flex justify-center mb-2">
+            <span className="flex-grow font-semibold">{item.name}</span>
+            <span className="w-1/4 text-right font-semibold mr-2">
+              {item.quantity}
+            </span>
+          </div>
+        ))}
+      </div>
+      {order.phoneNo && (
+        <p className="font-bold  text-left text-lg mb-1">
+          Phone No : {order.phoneNo}
+        </p>
+      )}
+      {order.address && (
+        <p className="font-bold  text-left text-lg mb-1">
+          Address : {order.address}
+        </p>
+      )}
+
+      <div className="my-3 flex justify-between">
+        <p className="font-bold   text-left text-lg mb-1">
+          {" "}
+          {formatPrice(order.payable_amount)}
+        </p>
+        {order.order_status != "IN_TRANSIT" && (
+          <button
+            type="button"
+            className=" btn-theme"
+            onClick={() => {
+              updateToCompleted(order.id);
+            }}
+          >
+            complete
+          </button>
+        )}
+        {order.order_status == "IN_TRANSIT" && (
+          <button
+            type="button"
+            id="verifybtn"
+            className=" btn-theme"
+            onClick={() => {
+              verifyOrder(order.userid, order.id);
+            }}
+          >
+            Verify
+          </button>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default OrderCard;
---
+++ frontend/src/pages/admin/ItemManager.tsx
@@ -0,0 +1,191 @@
+import { useEffect, useState } from "react";
+import { IItem, IItemScattered } from "../../types/interfaces";
+import ItemForm from "../../components/custom/Forms/ItemForm";
+import { openModal, closeModal } from "../../features/slices/modalSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../features/store";
+import PHModal from "../../components/custom/Modals/PHModal";
+import PHDataTable from "../../components/custom/DataTables/PHDataTable";
+import { nanoid } from "nanoid";
+import { useFetch } from "../../hooks/useFetch";
+import { ApiEndpoints } from "../../types/enums";
+import Swal from "sweetalert2";
+import { ObjectFlatter } from "../../utils/utils";
+
+const ItemManager = () => {
+  const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-right',
+    iconColor: 'white',
+    customClass: {
+      popup: 'colored-toast',
+    },
+    showConfirmButton: false,
+    timer: 1500,
+    timerProgressBar: true,
+  })
+  const dispatch = useDispatch()
+  const [isAdd, setisAdd] = useState(false)
+  const [isUpdate, setisUpdate] = useState(false)
+  const UpdateHook =  useFetch(import.meta.env.VITE_MANAGEMENT_SERVICE_URI+ ApiEndpoints.UPDATE_ITEM,"POST")
+  const DisplayHook = useFetch(import.meta.env.VITE_MANAGEMENT_SERVICE_URI+ ApiEndpoints.GET_ALL_ITEMS_ADMIN,"GET")
+  const DataAddHook =  useFetch(import.meta.env.VITE_MANAGEMENT_SERVICE_URI+ ApiEndpoints.ADD_ITEM,"POST")
+  const { isOpen } = useSelector((store: RootState) => {
+    return store.modal
+  }) 
+  
+  const [items, setItems] = useState<Item[]>([]);
+  const [itemupdate, setitemupdate] = useState<Item>({})
+  useEffect(() => {
+    console.log("Data fetchs")
+ setLatestItemData()  
+  }, [])
+  const setLatestItemData =()=>{
+   
+    DisplayHook.MakeHttpRequest().then((result)=>{
+    if(result.result){
+    const data = result.result.map((ot)=>{
+      ot.itemImage = JSON.stringify (ot.itemImage)
+      const so = ObjectFlatter(ot)
+      console.log(so)
+      so.itemImage =JSON.parse(so.itemImage)
+      return so
+    })
+    setItems(data)
+    }
+   })
+  } 
+ const handleAddItem = (item:any) => {
+    console.log(item)
+    DataAddHook.setPayload(item)
+    DataAddHook.MakeHttpRequest().then((result)=>{
+      console.log(result)
+      if(result.error || result.result.status==0){
+      Toast.fire({
+          title: "Error in inserting data !",
+          icon: "error"
+        });
+      } else{
+          Toast.fire({
+          icon: 'success',
+          title: 'Data Inserted !',
+        })
+        setLatestItemData() 
+        dispatch(closeModal())
+        setisAdd(false)
+
+      }
+    })
+    
+   
+   
+  };
+
+  const handleEdititem = (item) => {
+
+
+    setitemupdate(item)
+    setisUpdate(true)
+    dispatch(openModal())
+
+
+  };
+
+
+  const handleUpdate = (item) => {
+    UpdateHook.setPayload(item)
+    UpdateHook.MakeHttpRequest(item.id).then((result)=>{
+      if(result.error || result.result.status==0){
+        Toast.fire({
+            title: "Error in updating data !",
+            icon: "error"
+          });
+        } else{
+          setLatestItemData()
+          dispatch(closeModal())
+          setisUpdate(false)
+            Toast.fire({
+            icon: 'success',
+            title: 'Outlet Inserted !',
+          })
+         
+  
+        }
+    })
+
+    
+    dispatch(closeModal())
+    setisUpdate(false)
+  }
+  const handleDeleteitem = async (id: string) => {
+    let headersList = {
+      "Accept": "*/*",
+      "Content-Type":"application/json"
+      }
+     let response = await fetch(import.meta.env.VITE_MANAGEMENT_SERVICE_URI+ ApiEndpoints.DELETE_ITEM +id, { 
+       method: "DELETE",
+       headers: headersList
+     });
+     let data = await response.json();
+    if(data.error!=null){
+      Toast.fire({
+        icon: 'error',
+        title: 'Internal Error!!',
+      })
+    }else{
+       Toast.fire({
+        icon: 'success',
+        title: 'Data Deleted!!',
+      })
+    
+    }
+    setItems(items.filter((item) => item.id !== id));
+  };
+
+  return (
+    <div className="container px-4 py-10">
+      <div className="flex justify-between flex-wrap">
+        <button
+          className="btn-theme"
+          onClick={() => { setisAdd(true); dispatch(openModal()) }}
+        >
+          Add New item
+        </button>
+      </div>
+
+      {isAdd && <PHModal
+        isOpen={isOpen}
+        onClose={() => { setisAdd(false); dispatch(closeModal()) }}
+        headingText="Add item"
+        component={<ItemForm
+          update={null}
+          onEvent={handleAddItem}
+          action="Add item" />}
+      />}
+
+      <PHDataTable
+        title="Pizza Store Items"
+        data={items}
+        onDelete={handleDeleteitem}
+        onUpadate={handleEdititem}
+      />
+
+
+
+
+
+      {isUpdate && <PHModal
+        isOpen={isOpen}
+        onClose={() => { setisUpdate(false); dispatch(closeModal()) }}
+        headingText="Update item"
+        component={<ItemForm
+          onEvent={handleUpdate}
+          update={itemupdate}
+          action="Update item" />}
+      />}
+
+    </div>
+  )
+}
+
+export default ItemManager
